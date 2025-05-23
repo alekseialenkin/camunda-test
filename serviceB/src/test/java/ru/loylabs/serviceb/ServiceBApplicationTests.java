56f2b2c73bb27bd491cdbd20fd90b5489e6fce93
aleksei <aalenkin@loyalbs.ru>
2025-05-23 08:39:49 +0300

    init service b

---
+++ serviceB/src/test/java/ru/loylabs/serviceb/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.loylabs.serviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
